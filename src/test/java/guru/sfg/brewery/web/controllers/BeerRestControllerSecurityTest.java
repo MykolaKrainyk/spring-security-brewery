a1d50804cda80666b71c2ac456cde0aace216233
Mykola Krainyk <mykola_krainyk@epam.com>
2023-11-14 10:21:46 +0200

    override doFilter and successfulAuthentication methods

--- src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerSecurityTest.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerSecurityTest.java
@@ -3,6 +3,7 @@ package guru.sfg.brewery.web.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -12,10 +13,23 @@ public class BeerRestControllerSecurityTest extends BaseSecurityTest {
 
     @Test
     void deleteBeer() throws Exception {
-        mockMvc.perform(delete("/api/v1/beer/notValidUuid")
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
                 .header("Api-Key", "spring")
                 .header("Api-Secret", "security"))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteBeerHttpBasic() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
+                .with(httpBasic("spring", "security")))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    void deleteBeerNoAuth() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311"))
+                .andExpect(status().isUnauthorized());
     }
 
     @Test
@@ -26,8 +40,8 @@ public class BeerRestControllerSecurityTest extends BaseSecurityTest {
 
     @Test
     void findBeerById() throws Exception {
-        mockMvc.perform(get("/api/v1/beer/notValidUuid"))
-                .andExpect(status().isBadRequest());
+        mockMvc.perform(get("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311"))
+                .andExpect(status().isOk());
     }
 
     @Test
